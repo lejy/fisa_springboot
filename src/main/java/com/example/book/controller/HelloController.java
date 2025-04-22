ce6f33545ab5980bd1a3f98239d23b61c663c4c9
lejy <mbc09111@naver.com>
2025-04-22 14:01:55 +0900

    dd

---
+++ src/main/java/com/example/book/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.book.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+// URI와 메서드를 매핑하기 위한 프레젠테이션 계층
+//@Controller // MVC 아키텍쳐
+@RestController // REST API 형식으로 결과를 리턴
+@RequestMapping("/")
+public class HelloController {
+
+//    @GetMapping
+//    public String sayHello() {
+//        return "Hello SpringBoot!";
+//    }
+
+    @GetMapping("/{id}")
+    public String sayHello2(@PathVariable String id) {
+        return id + " SpringBoot!";
+    }
+
+    @GetMapping
+    public String sayHello(@RequestParam(value = "name", defaultValue = "Guest") String name) {
+        return "Hello, " + name + "!";
+    }
+}
